f20d4dc263d78f70037d9f55dbf639cd22340d27
宋高超 <songgaochao@meituan.com>
2015-08-11 20:11:51 +0800

    no message

---
+++ prototype/visit/static/js/init.js
@@ -0,0 +1,6 @@
+(function(){
+	var text = '<meta name="viewport" content="initial-scale=1, width=device-width, maximum-scale=1, user-scalable=no" />';
+	document.write(text);
+	document.documentElement.style.fontSize = "50px";
+}())
+	
